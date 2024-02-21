0f27a3a80d7f5657f58908fee4a59360d993f8ab
iMaddy <prshntsmp@gmail.com>
2024-02-21 20:38:19 +0400

    requirements files added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="iMaddy",
+    author_email="prshntsmp@gmail.com",
+    packages=find_packages()
+
+
+
+)
--- template.py
+++ template.py
@@ -4,9 +4,7 @@ from pathlib import Path
 
 project_name = "us_visa"
 list_of_files = {
-
     f"{project_name}/__init__.py",
-    
     f"{project_name}/components/__init__.py",
     f"{project_name}/components/data_ingestion.py",
     f"{project_name}/components/data_transformation.py",
@@ -14,26 +12,18 @@ list_of_files = {
     f"{project_name}/components/model_evaluation.py",
     f"{project_name}/components/model_trainer.py",
     f"{project_name}/components/model_pusher.py",
-
     f"{project_name}/configuration/__init__.py",
-    
     f"{project_name}/constant/__init__.py",
-
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
     f"{project_name}/entity/artifact_entity.py",
-
     f"{project_name}/exception/__init__.py",
-
     f"{project_name}/logger/__init__.py",
-
     f"{project_name}/pipeline/__init__.py",
     f"{project_name}/pipeline/train_pipeline.py",
     f"{project_name}/pipeline/prediction_pipeline.py",
-
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
-
     "app.py",
     "requirements.txt",
     "DockerFile",
@@ -42,8 +32,6 @@ list_of_files = {
     "setup.py",
     "config/model.yaml",
     "config/schema.yaml"
-
-
 }
 
 for file_path in list_of_files:
